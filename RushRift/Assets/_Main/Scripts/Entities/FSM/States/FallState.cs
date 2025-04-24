3930b30cc3712ffe0a1bc5f5b2bb04718a958d72
Lucas Campo <102192546+CampoLucas@users.noreply.github.com>
2025-04-24 17:45:26 -0300

    States
    * FallState
    * IdleState
    * JumpState
    * MoveState

---
+++ RushRift/Assets/_Main/Scripts/Entities/FSM/States/FallState.cs
@@ -0,0 +1,39 @@
+using Game.Entities.States;
+using UnityEngine;
+
+namespace Game.Entities.Components
+{
+    public class FallState : State<EntityArgs>
+    {
+        private GravityData _gravityData;
+
+        private float _gravity;
+        private float _velocity;
+
+        public FallState(GravityData gravityData)
+        {
+            _gravityData = gravityData;
+        }
+        
+        protected override void OnInit(ref EntityArgs args)
+        {
+            _gravity = _gravityData.GetValue();
+        }
+
+        protected override void OnStart(ref EntityArgs args)
+        {
+            if (!args.Controller.GetModel().TryGetComponent<IMovement>(out var movement)) return;
+            _velocity = movement.Velocity.y;
+        }
+
+        protected override void OnUpdate(ref EntityArgs args, float delta)
+        {
+            if (!args.Controller.GetModel().TryGetComponent<IMovement>(out var movement)) return;
+            _velocity += _gravity * delta;
+            var input = args.Controller.MoveDirection();
+            var dir = new Vector3(0, _velocity, 0);
+            movement.AddMoveDir(input);
+            movement.Move(dir, delta);
+        }
+    }
+}
