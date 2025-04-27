7549dd96e16bf327ca49fae091d16ff8ee48e264
Lucas Campo <102192546+CampoLucas@users.noreply.github.com>
2025-04-27 15:43:28 -0300

    Added the Level Manager

---
+++ RushRift/Assets/_Main/Scripts/_Managers/LevelManager.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using Game.DesignPatterns.Observers;
+using UnityEngine;
+
+namespace Game
+{
+    /// <summary>
+    /// Manages the level state, including win and lose conditions.
+    /// </summary>
+    [AddComponentMenu("Game/Level Manager")]
+    public class LevelManager : MonoBehaviour
+    {
+        private static LevelManager _instance;
+        
+        private ISubject _onGameOver = new Subject();
+        private ISubject _onLevelWon = new Subject();
+        private IObserver _onPlayerDeath;
+        private IObserver _onEnemyDeath;
+        
+        private int _allEnemies;
+        private int _deadEnemies;
+        private bool _gameOver;
+        
+        private void Awake()
+        {
+            _instance = this;
+            
+            _onPlayerDeath = new ActionObserver(OnPlayerDeath);
+            _onEnemyDeath = new ActionObserver(OnEnemyDeath);
+        }
+        
+        public static void GetPlayerReference(ISubject onDeath)
+        {
+            if (_instance == null)
+            {
+                return;
+            }
+
+            onDeath.Attach(_instance._onPlayerDeath);
+        }
+        
+        public static void GetEnemiesReference(ISubject onDeath)
+        {
+            if (_instance == null)
+            {
+                return;
+            }
+
+            onDeath.Attach(_instance._onEnemyDeath);
+            _instance._allEnemies += 1;
+        }
+        
+        public static bool TryGetGameOver(out ISubject subject)
+        {
+            if (_instance == null || _instance._onGameOver == null)
+            {
+                subject = null;
+                return false;
+            }
+
+            subject = _instance._onGameOver;
+            return true;
+        }
+        
+        public static bool TryGetLevelWon(out ISubject subject)
+        {
+            if (_instance == null || _instance._onLevelWon == null)
+            {
+                subject = null;
+                return false;
+            }
+
+            subject = _instance._onLevelWon;
+            return true;
+        }
+
+        public static bool IsGameOver()
+        {
+            if (_instance == null) return true;
+            return _instance._gameOver;
+        }
+
+        private void OnPlayerDeath()
+        {
+            _gameOver = true;
+            _onGameOver.NotifyAll();
+        }
+        
+        private void OnEnemyDeath()
+        {
+            _deadEnemies += 1;
+
+            if (_deadEnemies >= _allEnemies)
+            {
+                _onLevelWon.NotifyAll();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _onLevelWon.Dispose();
+            _onGameOver.Dispose();
+            _onPlayerDeath.Dispose();
+            _onEnemyDeath.Dispose();
+        }
+    }
+}
