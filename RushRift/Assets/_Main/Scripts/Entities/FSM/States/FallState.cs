a51fb052f8bb301898d63f7c13289b9f9548e430
Lucas Campo <102192546+CampoLucas@users.noreply.github.com>
2025-05-12 19:08:53 -0300

    Fixed heavy landing

--- RushRift/Assets/_Main/Scripts/Entities/FSM/States/FallState.cs
+++ RushRift/Assets/_Main/Scripts/Entities/FSM/States/FallState.cs
@@ -36,12 +36,15 @@ namespace Game.Entities
 
             var input = args.Controller.MoveDirection();
             var controlledInput = Vector3.Lerp(Vector3.zero, input, _gravityData.FallAirControl);
-
+            var jumpDir = Vector3.up * _velocity;
+            
             // Instead of separating input and vertical, COMBINE them
-            Vector3 finalMove = controlledInput * _gravityData.AirAcceleration + new Vector3(0, _velocity, 0);
+            //Vector3 finalMove = controlledInput * _gravityData.AirAcceleration + new Vector3(0, _velocity, 0);
 
+            movement.AddMoveDir(controlledInput);
+            movement.Move(jumpDir, delta);
             //movement.AddMoveDir(finalMove);
-            movement.Move(finalMove, delta);
+            //movement.Move(finalMove, delta);
         }
 
         protected override void OnExit(ref EntityArgs args)
