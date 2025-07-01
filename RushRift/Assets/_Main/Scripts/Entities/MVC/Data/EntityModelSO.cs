b11d317359e474bd1874284b8fb74b4850fdf355
Lucas Campo <102192546+CampoLucas@users.noreply.github.com>
2025-07-01 17:57:53 -0300

    Added coments to the base Entity MVC classes
    Commented classes and interfaces

--- RushRift/Assets/_Main/Scripts/Entities/MVC/Data/EntityModelSO.cs
+++ RushRift/Assets/_Main/Scripts/Entities/MVC/Data/EntityModelSO.cs
@@ -1,7 +1,14 @@
 namespace Game.Entities
 {
+    /// <summary>
+    /// Base ScriptableObject for creating runtime Model proxies
+    /// </summary>
     public abstract class EntityModelSO : SerializableSO
     {
+        /// <summary>
+        /// Creates a proxy instance to be used during runtime 
+        /// </summary>
+        /// <returns>A new model proxy</returns>
         public abstract NullCheck<IModel> GetProxy();
     }
 }
