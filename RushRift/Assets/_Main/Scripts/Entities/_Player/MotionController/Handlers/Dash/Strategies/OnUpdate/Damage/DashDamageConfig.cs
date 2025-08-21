c49c4874508c36177aa1b0c996b14fb750c22f0e
Lucas Campo <102192546+CampoLucas@users.noreply.github.com>
2025-08-21 16:30:27 -0300

    Settings changes

--- RushRift/Assets/_Main/Scripts/Entities/_Player/MotionController/Handlers/Dash/Strategies/OnUpdate/Damage/DashDamageConfig.cs
+++ RushRift/Assets/_Main/Scripts/Entities/_Player/MotionController/Handlers/Dash/Strategies/OnUpdate/Damage/DashDamageConfig.cs
@@ -6,14 +6,18 @@ namespace Game.Entities.Components.MotionController.Strategies
     [System.Serializable]
     public class DashDamageConfig
     {
-        [field:SerializeField] public bool InstaKill { get; private set; }
-        [field:SerializeField] public float Damage { get; private set; }
-        [field:SerializeField] public SphereOverlapData DetectionData { get; private set; }
-        public bool StopWhenKilling => stopWhenKillingEntities;
-        public bool StopWhenDestroying => stopWhenDestroyingEntities;
+        public bool InstaKill => instaKill; 
+        public float Damage => damage;
+        //[field:SerializeField] public SphereOverlapData DetectionData { get; private set; }
+        public bool StopOnKilling => stopOnDamage;
+        public bool StopOnDestroy => stopOnDestroy;
 
-        [Header("On Collision")]
-        [SerializeField] private bool stopWhenKillingEntities = true;
-        [SerializeField] private bool stopWhenDestroyingEntities = true;
+        [Header("Entities w/ health")]
+        [SerializeField] private bool instaKill = true;
+        [SerializeField] private float damage = 100f;
+        [SerializeField] private bool stopOnDamage = true;
+        
+        [Header("Destroyable entities")]
+        [SerializeField] private bool stopOnDestroy = true;
     }
 }
--- RushRift/Assets/_Main/Scripts/Entities/_Player/MotionController/Handlers/Dash/Strategies/OnUpdate/Damage/DashDamageStrategy.cs
+++ RushRift/Assets/_Main/Scripts/Entities/_Player/MotionController/Handlers/Dash/Strategies/OnUpdate/Damage/DashDamageStrategy.cs
@@ -31,69 +31,69 @@ namespace Game.Entities.Components.MotionController.Strategies
         {
             return false;
             
-            Debug.Log("SuperTest: Damage Dash update");
-            if (_detection == null)
-            {
-#if UNITY_EDITOR
-                Debug.Log("SuperTest: Detection created.");
-#endif
-                _detection = _config.DetectionData.Get(context.Origin);
-            }
-
-            _detection.Detect();
-
-            if (_detection.Overlaps > 0)
-            {
-                // Turn on effect
-                var collisions = _detection.Collisions;
-                for (var i = 0; i < _detection.Overlaps; i++)
-                {
-                    var other = collisions[i];
-
-                    if (other == null)
-                    {
-                        continue;
-                    }
-
-                    if (_damagedEntities.Contains(other.gameObject) || 
-                        !other.gameObject.TryGetComponent<IController>(out var controller))
-                    {
-                        continue;
-                    }
-                    
-                    // if (!other.gameObject.TryGetComponent<IController>(out var controller))
-                    // {
-                    //     continue;
-                    // }
-                    //
-                    // if (!controller.GetModel().TryGetComponent<HealthComponent>(out var health))
-                    // {
-                    //     continue;
-                    // }
-
-                    var model = controller.GetModel();
-                    if (model.TryGetComponent<HealthComponent>(out var health))
-                    {
-                        _damagedEntities.Add(other.gameObject);
-                        if (_config.InstaKill) health.Intakill(context.Position);
-                        else health.Damage(_config.Damage, context.Position);
-                    }
-                    else if (model.TryGetComponent<DestroyableComponent>(out var destroyable))
-                    {
-                        destroyable.DestroyEntity();
-                    }
-
-                    
-                }
-                
-                Debug.Log("SuperTest: Detected something");
-
-                return true;
-            }
-            // Turn off effect
-
-            Debug.Log("SuperTest: Detected nothing");
-            return false;
+//             Debug.Log("SuperTest: Damage Dash update");
+//             if (_detection == null)
+//             {
+// #if UNITY_EDITOR
+//                 Debug.Log("SuperTest: Detection created.");
+// #endif
+//                 _detection = _config.DetectionData.Get(context.Origin);
+//             }
+//
+//             _detection.Detect();
+//
+//             if (_detection.Overlaps > 0)
+//             {
+//                 // Turn on effect
+//                 var collisions = _detection.Collisions;
+//                 for (var i = 0; i < _detection.Overlaps; i++)
+//                 {
+//                     var other = collisions[i];
+//
+//                     if (other == null)
+//                     {
+//                         continue;
+//                     }
+//
+//                     if (_damagedEntities.Contains(other.gameObject) || 
+//                         !other.gameObject.TryGetComponent<IController>(out var controller))
+//                     {
+//                         continue;
+//                     }
+//                     
+//                     // if (!other.gameObject.TryGetComponent<IController>(out var controller))
+//                     // {
+//                     //     continue;
+//                     // }
+//                     //
+//                     // if (!controller.GetModel().TryGetComponent<HealthComponent>(out var health))
+//                     // {
+//                     //     continue;
+//                     // }
+//
+//                     var model = controller.GetModel();
+//                     if (model.TryGetComponent<HealthComponent>(out var health))
+//                     {
+//                         _damagedEntities.Add(other.gameObject);
+//                         if (_config.InstaKill) health.Intakill(context.Position);
+//                         else health.Damage(_config.Damage, context.Position);
+//                     }
+//                     else if (model.TryGetComponent<DestroyableComponent>(out var destroyable))
+//                     {
+//                         destroyable.DestroyEntity();
+//                     }
+//
+//                     
+//                 }
+//                 
+//                 Debug.Log("SuperTest: Detected something");
+//
+//                 return true;
+//             }
+//             // Turn off effect
+//
+//             Debug.Log("SuperTest: Detected nothing");
+//             return false;
         }
 
         public bool OnCollision(in MotionContext context, in Collider other)
@@ -112,14 +112,14 @@ namespace Game.Entities.Components.MotionController.Strategies
                 if (_config.InstaKill) health.Intakill(context.Position);
                 else health.Damage(_config.Damage, context.Position);
 
-                return _config.StopWhenKilling;
+                return _config.StopOnKilling;
             }
             
             if (model.TryGetComponent<DestroyableComponent>(out var destroyable))
             {
                 destroyable.DestroyEntity();
 
-                return _config.StopWhenDestroying;
+                return _config.StopOnDestroy;
             }
 
             return true;
