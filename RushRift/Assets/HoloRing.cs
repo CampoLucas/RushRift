649cb59c558254a1acd3b183b8e74704977f9bdc
Camila Liendro <81248814+camiagus2001@users.noreply.github.com>
2025-09-03 12:37:39 -0300

    Ring Led Movement

--- RushRift/Assets/HoloRing.cs
+++ RushRift/Assets/HoloRing.cs
@@ -6,7 +6,7 @@ public class HoloRing : MonoBehaviour
 
     void Update()
     {
-        // Rota sobre el eje Y, es decir, alrededor
+       
         transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
     }
 }
