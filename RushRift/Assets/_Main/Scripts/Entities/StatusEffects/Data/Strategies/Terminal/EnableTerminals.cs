be14b087f8496fabaa3eb7d43db010ab64b222cd
Lucas Campo <102192546+CampoLucas@users.noreply.github.com>
2026-04-01 15:53:58 -0300

    Added electricity vfx to the hand

--- RushRift/Assets/_Main/Scripts/Entities/StatusEffects/Data/Strategies/Terminal/EnableTerminals.cs
+++ RushRift/Assets/_Main/Scripts/Entities/StatusEffects/Data/Strategies/Terminal/EnableTerminals.cs
@@ -5,14 +5,18 @@ namespace Game.Entities
     [CreateAssetMenu(fileName = "EnableTerminals", menuName = "Game/Effects/Strategies/Enable Terminals")]
     public class EnableTerminals : EffectStrategy
     {
+        private static readonly int PowerUp = Shader.PropertyToID("PowerSurge");
+
         public override void StartEffect(IController controller)
         {
             GlobalLevelManager.SetPowerSurge(true);
+            Shader.SetGlobalFloat(PowerUp, 1);
         }
 
         public override void StopEffect(IController controller)
         {
             GlobalLevelManager.SetPowerSurge(false);
+            Shader.SetGlobalFloat(PowerUp, 0);
         }
     }
 }
